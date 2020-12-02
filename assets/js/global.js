13422f8610cca7ad39113997414ff5a5524116dc
JohanaLl <johisllanos@gmail.com>
2020-12-01 19:07:26 -0500

    Se finaliza crud

---
+++ assets/js/global.js
@@ -0,0 +1,6 @@
+export var GLOBAL = {
+    //url: '/bancoSoft/services/api'
+    url: 'https://bancoapisoft.azurewebsites.net/bancoSoft/services/api',
+    //url: 'https://bancosoftii7api.azurewebsites.net/bancoSoft/services/api',
+    //url:'http://localhost:8080/bancoSoft/services/api/' 
+}
